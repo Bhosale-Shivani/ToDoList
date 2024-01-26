ae8cba523cf5be21d4923656f57aae3bddb5923f
Bhosale-Shivani <shivanibhosale1830@gmail.com>
2024-01-26 23:37:16 +0530

    add all files

---
+++ render.js
@@ -0,0 +1,7 @@
+import store from "./store"
+
+function render()
+{
+    console.log(store);
+}
+export default render
---
+++ store.js
@@ -0,0 +1,15 @@
+const store = {
+todos:[
+    {
+        id: "1",
+        title: "Complete Task A",
+        completed:false,
+    },
+    {
+        id:"2",
+        title:"complete Task A",
+        completed: true,
+    },
+],
+}
+export default store
